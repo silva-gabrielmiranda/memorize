8f352709f7b6111b66fbf99ca41aea2d2b3186f5
Gabriel Miranda <silva.gabrielmiranda@gmail.com>
2021-11-28 12:23:06 -0300

    Initial Commit

---
+++ Memorize2021/Memorize2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2021App.swift
+//  Memorize2021
+//
+//  Created by Gabriel Miranda on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
